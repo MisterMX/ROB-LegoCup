6ea306c3d416d8bdeb583ec25e8ec11b587c431d
student <student>
2017-05-29 13:18:03 +0200

    Implement searching balls

--- colors.c
+++ colors.c
@@ -1,22 +1,5 @@
 #include "colors.h"
 
-TLegoColors getColorNameByHSB(tSensors nDeviceIndex, int lineNumber)
-{
-	long hue = getColorHue(nDeviceIndex);
-	//long sat = getColorSaturation(nDeviceIndex);
-	short val = getColorReflected(nDeviceIndex);
-
-	displayString(lineNumber, "H: %d3; S: %d3; V: %d", hue, 0, val);
-
-	if(hue > 20 && hue < 35)
-		return colorWhite;
-	if(hue >= 35 && hue < 70)
-		return colorBlack;
-	if(hue > 110 && hue < 130)
-		return colorGreen;
-	return colorNone;
-}
-
 TLegoColors getLegoColorFromRGB(tSensors nDevice, int lineNumber)
 {
 	long red;
--- colors.h
+++ colors.h
@@ -1,7 +1,6 @@
 #ifndef __COLORS__
 #define __COLORS__
 
-TLegoColors getColorNameByHSB(tSensors nDeviceIndex, int lineNumber);
 TLegoColors getLegoColorFromRGB(tSensors nDevice, int lineNumber);
 
 #endif
